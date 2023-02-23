e5120a1aac61aa38edf6aa02f7f78144b2fe12b3
AlexVYard <alexvegayard@gmail.com>
2023-02-23 13:34:18 -0500

    funcinalidad de registro implementada
    3 errores como este: Unable to resolve path to module 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js'

--- src/components/registro.js
+++ src/components/registro.js
@@ -1,3 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
+import { getDatabase, set, ref } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 export const registro = () => {
   const root = document.getElementById('pantallaMostrada');
   root.innerHTML = `
@@ -13,4 +20,51 @@ export const registro = () => {
       </section>
     </main>
   `;
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: 'AIzaSyA4DCQlvHVQ8XYZsIWz5GkEoExfeJsH30s',
+    authDomain: 'testsocialnetwork0-b5d33.firebaseapp.com',
+    projectId: 'testsocialnetwork0-b5d33',
+    storageBucket: 'testsocialnetwork0-b5d33.appspot.com',
+    messagingSenderId: '235016872717',
+    appId: '1:235016872717:web:31faf95a85c2e8da0cc644',
+    measurementId: 'G-DJP89WLW09',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const database = getDatabase(app);
+
+  document.getElementById('botonInicio').addEventListener('click', () => {
+    const email = document.getElementById('inputEmail').value;
+    const password = document.getElementById('inputPassword').value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        set(ref(database, `users/${user.uid}`), {
+          email,
+          password,
+        })
+          .then(() => {
+            // Data saved successfully!
+            alert('Usuario en la base de datos!');
+          })
+          .catch((error) => {
+            // The write failed...
+            alert(error);
+          });
+        alert('Usuario creado!');
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        alert(errorMessage);
+      });
+  });
 };
