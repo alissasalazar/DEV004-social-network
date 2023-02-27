2420baf55318ab754a5770083936dca0c3b41a2f
AlexVYard <alexvegayard@gmail.com>
2023-02-27 08:21:56 -0500

    funcionalidad de login implementada

--- src/components/loginEventos.js
+++ src/components/loginEventos.js
@@ -1,4 +1,9 @@
+import { signIn } from '../lib/signIn.js';
+
 // eventos del login-dom
 export const loginEventos = (onNavigate) => {
   document.getElementById('botonRegistrar').addEventListener('click', () => onNavigate('/registro'));
+  document.getElementById('botonInicio').addEventListener('click', () => {
+    signIn();
+  });
 };
