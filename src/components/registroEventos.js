16ba123f3f61d4fe7956e9e25a0d975f58aa1fcf
AlexVYard <alexvegayard@gmail.com>
2023-02-27 13:03:28 -0500

    corregido error para registrarse

--- src/components/registroEventos.js
+++ src/components/registroEventos.js
@@ -2,6 +2,13 @@ import { signUp } from '../lib/signUp.js';
 
 // eventos del registro-dom
 export const registroEventos = (onNavigate) => {
+  document.getElementById('inputEmail').addEventListener('keyup', function() {
+    if (document.getElementById('inputEmail').value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      document.getElementById('botonInicio').style['pointer-events'] = "all";
+    } else {
+      document.getElementById('botonInicio').style['pointer-events'] = "none"
+    }
+  });
   document.getElementById('botonInicioGoogle').addEventListener('click', () => onNavigate('/timeline'));
   document.getElementById('botonInicio').addEventListener('click', () => {
     signUp();
