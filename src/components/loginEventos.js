f53049585d5ee5a4306b930cfcb37d76c795f86e
AlexVYard <alexvegayard@gmail.com>
2023-02-27 12:50:57 -0500

    deteccion de correo en la casilla inputEmail

--- src/components/loginEventos.js
+++ src/components/loginEventos.js
@@ -2,6 +2,14 @@ import { signIn } from '../lib/signIn.js';
 
 // eventos del login-dom
 export const loginEventos = (onNavigate) => {
+  // deteccion de correo en la casilla inputEmail
+  document.getElementById('inputEmail').addEventListener('keyup', function() {
+    if (document.getElementById('inputEmail').value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      document.getElementById('botonInicio').style['pointer-events'] = "all";
+    } else {
+      document.getElementById('botonInicio').style['pointer-events'] = "none"
+    }
+  });
   document.getElementById('botonRegistrar').addEventListener('click', () => onNavigate('/registro'));
   document.getElementById('botonInicio').addEventListener('click', () => {
     signIn();
