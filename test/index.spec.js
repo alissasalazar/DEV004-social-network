aad79a955dc7c9c1c17f378ffde95b378134b6e6
AlexVYard <alexvegayard@gmail.com>
2023-03-01 16:56:53 -0500

    modified:   test/index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,15 @@
 // importamos la funcion que vamos a testear
 import { myFunction } from '../src/lib/index';
+import { signIn } from '../src/lib/signIn.js';
 
 describe('myFunction', () => {
   it('debería ser una función', () => {
     expect(typeof myFunction).toBe('function');
+  });
+});
+
+describe('signIn', () => {
+  it('debería ser una función', () => {
+    expect(typeof signIn).toBe('function');
   });
 });
