23bdde8a5e2612afdc606c74e1f5b5c9f83b1be2
AlexVYard <alexvegayard@gmail.com>
2023-03-06 11:04:40 -0500

    agregados botones editar (falta funcionalidad)
    enlist modificado
    agrupados todos los botones editar en un nodo
    agregados botones editar
    css modificado para estilos de botones editar

--- src/components/timelineEventos.js
+++ src/components/timelineEventos.js
@@ -5,4 +5,13 @@ export const timelineEventos = async (onNavigate) => {
   const mainPublicacion = document.getElementById('miPublicacion');
   mainPublicacion.innerHTML += await firebaseLeerPublicacion();
   document.getElementById('nuevaPublicacion').addEventListener('click', () => onNavigate('/crear-publicacion'));
+  
+  const muro = document.getElementById('miPublicacion')
+  console.log(muro)
+  
+  const botonesEditar = muro.querySelectorAll('.botonEditar')
+  // botonEditar.forEach(callbackfn => {
+  //   console.log(callbackfn)
+  // })
+  console.log(botonesEditar)
 };
