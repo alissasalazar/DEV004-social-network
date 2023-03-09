a851c44a0b99f7c8b10b1dec7611b1f3b81ff3af
YesicaChui <yesicachuic@gmail.com>
2023-03-08 19:54:55 -0500

    verificando que una publicacion no este vacia

--- src/components/crearPublicacionEventos.js
+++ src/components/crearPublicacionEventos.js
@@ -8,6 +8,10 @@ export const crearPublicacionEventos = (onNavigate) => {
       console.log('llamado firebase grupal');
       const textoPublicacion = document.getElementById('textoPublicacion');
       console.log(textoPublicacion.value);
+      if(textoPublicacion.value == "")  {
+        alert("Por favor escriba algun texto para publicar")
+        return
+      }
       await firebaseCrearPublicacion(textoPublicacion.value);
       alert('se inserto la publicacion');
       onNavigate('/timeline');
