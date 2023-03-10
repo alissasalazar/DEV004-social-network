7f2085538693d33011b92eb1fb7860f520461f5b
AlexVYard <alexvegayard@gmail.com>
2023-03-10 13:28:48 -0500

    barriles unidos

--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -2,19 +2,19 @@
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
 // mporto la configuracion de firebase
-import {
-  getAuth, createUserWithEmailAndPassword, getDatabase, set, ref,
-} from './barrel.js';
-import FirebaseConfig from '../firebaseConfig.js';
-
-const { app: FirebaseApp } = FirebaseConfig;
+import Firebase from "../firebaseConfig.js";
+// pongo los nombres usuales de los objetos y funciones de firebase
+const { 
+  auth, createUserWithEmailAndPassword, set, ref, database,
+} = Firebase;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-export const auth = getAuth();
 export const signUp = async (email, password) => {
   // Initialize Firebase
-  const database = getDatabase(FirebaseApp);
+  // const database = getDatabase(FirebaseApp);
 
   try {
     const credentialsUser = await createUserWithEmailAndPassword(auth, email, password)
